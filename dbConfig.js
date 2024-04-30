5322b4589c857a5d5bd3ed455d73d2bb0be130fc
franilones <franolmosdev@gmail.com>
2024-05-01 01:41:28 +0200

    Get con Sequelize

--- dbConfig.js
+++ dbConfig.js
@@ -12,8 +12,8 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
-    "root",
-    "BgTHh$32Ww",
-    {host: "127.0.0.1", dialect: "mysql"});
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {host: process.env.DB_HOST, dialect: "mysql"});
 
 module.exports = sequelize;
